1b4566ae8c4550007f96cf8e770c413d1f16b298
AteneaC <Jvcatalina@gmail.com>
2022-08-12 12:34:48 -0400

    Predicciones

---
+++ Quilodran_Javiera_JSPredicción.js
@@ -0,0 +1,17 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+// el console.log imprime Naci en 1980
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+// el console.log imprime Naci en 1980  
+function add(num1, num2){
+        console.log("¡Sumando números!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    
+    // el console.log imprime sumando numeros, el valor de num1 y num2, el valor 30
