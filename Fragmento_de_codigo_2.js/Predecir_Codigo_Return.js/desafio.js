3ba922d04a87d74f6a0f8853935254884b25648b
AteneaC <Jvcatalina@gmail.com>
2022-08-26 11:40:35 -0400

    desafio de codigo

---
+++ Fragmento_de_codigo_2.js/Predecir_Codigo_Return.js/desafio.js
@@ -0,0 +1,10 @@
+var alturaNiño = 80;
+    function muestraSiElNiñoPuedeSubirALaMontañaRusa (alturaniño) {
+        if (alturaniño > 52){
+            console.log ("¡Súbete, chico!");
+        }
+        else {
+            console.log ("Lo siento, chico Tal vez el próximo año");
+        }
+    }
+    // console.log imprimira "subete chico"
