c418b39c82fa21fda754be72a3f37491cade7b63
AteneaC <jvcatalina.iccp@gmail.com>
2023-04-21 10:56:25 -0400

    ok

---
+++ MeGusta/Segundo/segundo.js
@@ -0,0 +1,32 @@
+var count1 = 9;
+var count1Element = document.querySelector("#count1");
+
+console.log(count1Element);
+
+function MeGusta1() {
+    count1++;
+    count1Element.innerText = count1 + " like(s)";
+    console.log(count1);
+}
+
+var count2 = 12;
+var count2Element = document.querySelector("#count2");
+
+console.log(count2Element);
+
+function MeGusta2() {
+    count2++;
+    count2Element.innerText = count2 + " like(s)";
+    console.log(count2);
+}
+
+var count3 = 9;
+var count3Element = document.querySelector("#count3");
+
+console.log(count3Element);
+
+function MeGusta3() {
+    count3++;
+    count3Element.innerText = count3 + " like(s)";
+    console.log(count3);
+}
